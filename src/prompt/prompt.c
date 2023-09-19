6070e270809848b44e1484ef4a4704de8ca7b9e1
Salah <salah.bouheniche@gmail.com>
2023-09-19 07:18:44 +0200

    Add command history

--- src/prompt/prompt.c
+++ src/prompt/prompt.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 00:07:11 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/09/11 13:35:13 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/09/19 07:17:14 by joakoeni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ void	read_user_input(t_shell *shell)
 		input = readline(shell->prompt);
 		if (input)
 		{
+			if (*input)
+				add_history(input);
 			if (!g_sigquit_received)
 				shell->tokens = extract_tokens(input);
 			free(input);
@@ -30,4 +32,5 @@ void	read_user_input(t_shell *shell)
 		if (!input)
 			shell->status = 0;
 	}
+	clear_history();
 }
