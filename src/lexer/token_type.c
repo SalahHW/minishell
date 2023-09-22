b74ddbec74f1c8860d3836b805dcbb7dd122d7f5
Salah <salah.bouheniche@gmail.com>
2023-09-22 05:55:56 +0200

    Update type detection to use doubly linked list

--- src/lexer/token_type.c
+++ src/lexer/token_type.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 04:43:28 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/09/22 04:46:16 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/09/22 05:52:44 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,31 @@
 
 void	detect_tokens_type(t_tokenlist *tokens)
 {
-	t_token		*token_ptr;
-	t_tokentype	prev_type;
+	t_token	*token_ptr;
 
-	prev_type = token_cmd;
 	token_ptr = tokens->head;
-	token_ptr->type = token_cmd;
 	while (token_ptr)
 	{
-		if (prev_type == token_cmd || prev_type == token_arg)
-		{
-			if (token_ptr->value[0] == '|' && token_ptr->value[1] == 0)
-				token_ptr->type = token_pipe;
-			else if (token_ptr->value[0] == '<' && token_ptr->value[1] == 0)
-				token_ptr->type = token_redirect_in;
-			else if (token_ptr->value[0] == '>' && token_ptr->value[1] == 0)
-				token_ptr->type = token_redirect_out;
-			else if (token_ptr->value[0] == '>' && token_ptr->value[1] == '>')
-				token_ptr->type = token_redirect_out;
-			else if (token_ptr->value[0] == '<' && token_ptr->value[1] == '<')
-				token_ptr->type = token_redirect_append;
-			else if (prev_type == token_cmd)
-				token_ptr->type = token_arg;
-			else
-				token_ptr->type = token_cmd;
-		}
+		if (token_ptr->value[0] == '|' && token_ptr->value[1] == 0)
+			token_ptr->type = token_pipe;
+		else if (token_ptr->value[0] == '<' && token_ptr->value[1] == 0)
+			token_ptr->type = token_redirect_in;
+		else if (token_ptr->value[0] == '>' && token_ptr->value[1] == 0)
+			token_ptr->type = token_redirect_out;
+		else if (token_ptr->value[0] == '>' && token_ptr->value[1] == '>')
+			token_ptr->type = token_redirect_append;
+		else if (token_ptr->value[0] == '<' && token_ptr->value[1] == '<')
+			token_ptr->type = token_redirect_append;
 		else
-			token_ptr->type = token_cmd;
-		prev_type = token_ptr->type;
+		{
+			if (!token_ptr->prev || token_ptr->prev->type == token_pipe
+				|| token_ptr->prev->type == token_redirect_in
+				|| token_ptr->prev->type == token_redirect_out
+				|| token_ptr->prev->type == token_redirect_append)
+				token_ptr->type = token_cmd;
+			else
+				token_ptr->type = token_arg;
+		}
 		token_ptr = token_ptr->next;
 	}
 }
