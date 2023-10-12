5fcfe063484e248258a42616ea29de1496b2aa9a
Salah <salah.bouheniche@gmail.com>
2023-10-12 17:13:27 +0200

    Split main tokenizer function for cleaner code

--- src/lexer/tokenizer.c
+++ src/lexer/tokenizer.c
@@ -6,20 +6,29 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 10:48:26 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/10/12 13:54:18 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/10/12 16:29:19 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-// Scan the input and return a list of tokens
-t_tokenlist	*tokenize_input(char *input)
+t_tokenlist *tokenizer(char *input)
 {
-	char		*input_ptr;
 	t_tokenlist	*tokens;
 
-	input_ptr = input;
 	tokens = init_tokens_list();
+	tokens = tokenize_input(input, tokens);
+	detect_tokens_type(tokens);
+	print_tokens_list(tokens);
+	return (tokens);	
+}
+
+// Scan the input and return a list of tokens
+t_tokenlist	*tokenize_input(char *input, t_tokenlist *tokens)
+{
+	char		*input_ptr;
+
+	input_ptr = input;
 	while (*input_ptr)
 	{
 		if (*input_ptr == '\'' || *input_ptr == '\"')
@@ -31,7 +40,6 @@ t_tokenlist	*tokenize_input(char *input)
 		else
 			input_ptr = tokenize_word(input_ptr, tokens);
 	}
-	print_tokens_list(tokens);
 	return (tokens);
 }
 
