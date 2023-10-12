818cd6b1f7a8a46aee7c33d5bd563684a9fe9d6d
Salah <salah.bouheniche@gmail.com>
2023-10-12 17:08:58 +0200

    Add folder for testings functions

---
+++ src/test/print_tokens.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_tokens.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/12 16:31:31 by sbouheni          #+#    #+#             */
+/*   Updated: 2023/10/12 16:36:07 by sbouheni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+// Print the tokens list
+void	print_tokens_list(t_tokenlist *tokens)
+{
+	t_token	*token_ptr;
+
+	token_ptr = tokens->head;
+	while (token_ptr)
+	{
+		printf("Token: %s\n", token_ptr->value);
+		if (token_ptr->type == t_undifined)
+			printf("Type: undifined\n");
+		else if (token_ptr->type == t_cmd)
+			printf("Type: command\n");
+		else if (token_ptr->type == t_arg)
+			printf("Type: argument\n");
+		else if (token_ptr->type == t_pipe)
+			printf("Type: pipe\n");
+		else if (token_ptr->type == t_redirect_in)
+			printf("Type: redirect in\n");
+		else if (token_ptr->type == t_redirect_out)
+			printf("Type: redirect out\n");
+		else if (token_ptr->type == t_heredoc)
+			printf("Type: heredoc\n");
+		else if (token_ptr->type == t_file)
+			printf("Type: file\n");
+		token_ptr = token_ptr->next;
+	}
+}
