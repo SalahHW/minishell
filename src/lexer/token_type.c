e2fa60c8ee9ee957444283b5250d0dc259f06ddb
Salah <salah.bouheniche@gmail.com>
2023-10-23 09:20:38 +0200

    Add t_exit_status type

--- src/lexer/token_type.c
+++ src/lexer/token_type.c
@@ -6,7 +6,7 @@
 /*   By: aherrman <aherrman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 04:43:28 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/10/18 09:19:11 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/10/23 07:19:13 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ static t_tokentype	get_token_type(t_token *token)
 		return (t_redirect_append);
 	if (ft_strncmp(token->value, "<<", 3) == 0)
 		return (t_heredoc);
+	if (ft_strncmp(token->value, "$?", 3) == 0)
+		return (t_exit_status);
 	if (!token->prev || token->prev->type == t_pipe)
 		return (t_cmd);
 	if (token->prev->type == t_redirect_in
