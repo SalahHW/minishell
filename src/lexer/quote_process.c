2132f1e72a7514e594f6bc86d2128e87303ea0dc
Salah <salah.bouheniche@gmail.com>
2023-10-26 07:08:46 +0200

    Fix norm

--- src/lexer/quote_process.c
+++ src/lexer/quote_process.c
@@ -6,15 +6,15 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 22:24:46 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/10/13 15:21:26 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/10/26 06:51:09 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void remove_quotes(t_token *token)
+void	remove_quotes(t_token *token)
 {
-	char *new_value;
+	char	*new_value;
 
 	new_value = ft_substr(token->value, 1, ft_strlen(token->value) - 2);
 	replace_token(token, new_value);
--- src/lexer/token_list.c
+++ src/lexer/token_list.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 05:08:15 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/10/17 14:25:59 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/10/26 06:52:50 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ void	add_new_token(t_tokenlist *tokens, char *value)
 	tokens->tail = new_token;
 }
 
-void replace_token(t_token *token, char *new_value)
+void	replace_token(t_token *token, char *new_value)
 {
 	free(token->value);
 	token->value = new_value;
