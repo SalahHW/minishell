4c1b264283b9e9c809e91c7f22bdef4e9f3396a8
Salah <salah.bouheniche@gmail.com>
2023-10-26 07:04:25 +0200

    Remove testing function

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -6,34 +6,11 @@
 /*   By: aherrman <aherrman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 13:38:03 by aherrman          #+#    #+#             */
-/*   Updated: 2023/10/23 10:11:28 by aherrman         ###   ########.fr       */
+/*   Updated: 2023/10/26 06:44:21 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-
-void	ft_print_token_list(t_token *token)
-{
-	while (token)
-	{
-		printf("token->value = %s\n", token->value);
-		printf("token->type = %d\n", token->type);
-		printf(" fd in = %d fd out = %d\n", token->fd_in, token->fd_out);
-		printf("fd value %d\n", token->fd);
-		token = token->next;
-	}
-}
-
-void	ft_print_execlist(t_execlist *execlist)
-{
-	while (execlist)
-	{
-		printf("cmd_path = %s\n", execlist->cmd_path);
-		printf("fd in = %d fd out = %d\n", execlist->fd_in, execlist->fd_out);
-		ft_print_tab_for_test(execlist->arg);
-		execlist = execlist->next;
-	}
-}
 
 int	ft_for_builtins(char *str)
 {
@@ -92,7 +69,6 @@ int	ft_only_one_cmd(t_shell *shell)
 		{
 			if (ft_solo_child(shell) == 1)
 				return (1);
-			// ERR solochild
 			waitpid(-1, &statut, 0);
 		}
 	}
@@ -111,11 +87,9 @@ int	ft_multi_cmd(t_shell *shell, int nbprocess)
 			return (1);
 		if (ft_for_builtins(shell->execlist->arg[0]) == 1)
 			exec_builtins(shell, i);
-		// ERROR FORK//
 		else if (shell->general->pids[i] == 0)
 			if (ft_child_process(shell, i) == 1)
 				return (1);
-		// error on child
 		if (shell->execlist->next)
 			shell->execlist = shell->execlist->next;
 		i++;
