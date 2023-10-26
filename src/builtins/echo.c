f8ed99aaa998ff7d760c8226a4ca366b4a78474e
alexandre <alexandre.herrmann.67@gmail.com>
2023-10-26 16:45:22 +0200

    ok multipipes + multi redir + heredoc

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: aherrman <aherrman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 09:56:41 by aherrman          #+#    #+#             */
-/*   Updated: 2023/10/17 16:27:38 by aherrman         ###   ########.fr       */
+/*   Updated: 2023/10/26 15:12:45 by aherrman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,15 @@ int	ft_echo(t_shell *exec)
 {
 	int	n;
 
-	if (ft_strncmp(exec->execlist->arg[1], "-n", 3) == 0)
+	if (exec->execlist->arg[1] && ft_strncmp(exec->execlist->arg[1], "-n",
+			3) == 0)
 		n = 1;
 	else
 		n = 0;
-	if (exec->execlist->arg[1] && n == 1)
+	if (n == 1)
 		printf("%s", ft_tab_on_one_line(exec->execlist->arg + 2));
-	else if (exec->execlist->arg[2])
+	else
 		printf("%s\n", ft_tab_on_one_line(exec->execlist->arg + 1));
-	else if (exec->execlist->arg[2] == NULL)
-	{
-		if (n == 1)
-			printf("\n");
-		// new line
-		else
-			return (0);
-	}
 	return (0);
 }
 // PERROR a lire
