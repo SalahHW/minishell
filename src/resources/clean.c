a1cf6de3fd1ee7f6582bef6f62efb1faf9670fc7
Salah <salah.bouheniche@gmail.com>
2023-10-26 07:14:25 +0200

    Remove unused functions

--- src/resources/clean.c
+++ src/resources/clean.c
@@ -6,39 +6,15 @@
 /*   By: aherrman <aherrman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 01:42:05 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/10/23 10:32:48 by aherrman         ###   ########.fr       */
+/*   Updated: 2023/10/25 16:59:20 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void	close_all_fd(t_shell *shell)
-{
-	t_token	*tmp;
-
-	tmp = shell->tokens->head;
-	if (!tmp)
-		return ;
-	while (tmp)
-	{
-		if (tmp->type == t_file)
-			close(tmp->fd);
-		tmp = tmp->next;
-	}
-}
-
-void	free_for_general(t_shell *shell)
-{
-	free_tab(shell->general->env);
-	free_tab(shell->general->path);
-	free_tab(shell->general->expt);
-	free(shell->general->home);
-	free(shell->general);
-}
-
 void	clean_shell(t_shell *shell)
 {
-	close_all_fd(shell);
-	free_for_general(shell);
 	free(shell->prompt);
+	free(shell->general);
+	clear_environement_list(shell->environement_list);
 }
