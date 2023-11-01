164db6f37e32e2f5628b0fdd49aa3320efa91642
alexandre <alexandre.herrmann.67@gmail.com>
2023-11-01 10:28:40 +0100

    echo ok MAJ pour env dans exec (qq leak a voir) unset nok

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: aherrman <aherrman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 09:56:41 by aherrman          #+#    #+#             */
-/*   Updated: 2023/10/31 14:12:58 by aherrman         ###   ########.fr       */
+/*   Updated: 2023/11/01 09:55:46 by aherrman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,28 @@
 int	ft_echo(t_shell *exec)
 {
 	int	n;
+	int	i;
 
 	if (exec->execlist->arg[1] && ft_strncmp(exec->execlist->arg[1], "-n",
 			3) == 0)
+	{
 		n = 1;
+		i = 2;
+	}
 	else
+	{
+		i = 1;
 		n = 0;
-	if (n == 1)
-		printf("%s ", ft_tab_on_one_line(exec->execlist->arg + 2));
-	else
-		printf("%s \n", ft_tab_on_one_line(exec->execlist->arg + 1));
+	}
+	while (exec->execlist->arg[i])
+	{
+		ft_putstr_fd(exec->execlist->arg[i], 1);
+		if (ft_tab_size(exec->execlist->arg) > i + 1)
+			write(1, " ", 1);
+		i++;
+	}
+	if (n == 0)
+		write(1, "\n", 1);
 	return (0);
 }
 // PERROR a lire
--- src/format_for_exec/main_format.c
+++ src/format_for_exec/main_format.c
@@ -6,7 +6,7 @@
 /*   By: aherrman <aherrman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 14:44:42 by aherrman          #+#    #+#             */
-/*   Updated: 2023/10/30 13:56:01 by aherrman         ###   ########.fr       */
+/*   Updated: 2023/11/01 10:00:37 by aherrman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,24 +94,20 @@ int	ft_create_list(t_shell *shell)
 int	format_for_exec(t_shell *shell)
 {
 	int cmd;
-
+	ft_create_env_and_path(shell);
 	cmd = 0;
 	if (ft_format_for_tokens(shell) == 1)
 		return (1);
-	// ERR format for tokens//
 	if (ft_create_list(shell) == 1)
 		return (1);
 	cmd = ft_lst_len(shell->execlist);
-	// ERR create execlist
 	if (cmd != 0)
 	{
 		if (ft_redirections(shell) == 1)
 			return (1);
-		// ERR redir fd
 		if (ft_general_pipe(shell) == 1)
 			return (1);
 	}
-	// ERR create pipes
 	ft_h(shell);
 	return (0);
 }
