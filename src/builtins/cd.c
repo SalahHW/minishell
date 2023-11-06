c3696bf01ca8ee0770e8607d1830a7d6c334862e
alexandre <alexandre.herrmann.67@gmail.com>
2023-11-06 13:21:55 +0100

    upgrade

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,29 +6,12 @@
 /*   By: aherrman <aherrman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 09:10:35 by aherrman          #+#    #+#             */
-/*   Updated: 2023/11/03 06:54:37 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/11/06 11:22:32 by aherrman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-// int	ft_cd(t_shell *exec)
-// {
-// 	(void)exec;
-// 	// int	err;
-// 	// err = 0;
-// 	// if (exec->execlist->arg == NULL && ft_search_in_tab(exec->general->env,
-// 	//	"HOME") != NULL)
-// 	// 	return (1);
-// 	// else if (exec->execlist->arg[1] == NULL)
-// 	// 	err = chdir(exec->general->home);
-// 	// else
-// 	// 	err = chdir(exec->execlist->arg[1]);
-// 	// if (err != 0)
-// 	// 	return (1);
-// 	// pwd_change(exec);
-// 	return (0);
-// }
 int	ft_cd(t_shell *exec)
 {
 	int		err;
@@ -36,16 +19,17 @@ int	ft_cd(t_shell *exec)
 
 	err = 0;
 	home = get_var_value(exec->environement_list, "HOME");
-	if (home == NULL)
+	if (home == NULL && exec->execlist->arg[1] == NULL)
 	{
 		error("cd", NULL, 3);
 		return (1);
 	}
 	else if (exec->execlist->arg[1] == NULL)
 		err = chdir(home);
-	err = chdir(exec->execlist->arg[1]);
+	else
+		err = chdir(exec->execlist->arg[1]);
 	if (err != 0)
 		return (1);
-	//pwd_change(exec);
+	// pwd_change(exec);
 	return (0);
 }
--- src/error/error.c
+++ src/error/error.c
@@ -6,7 +6,7 @@
 /*   By: aherrman <aherrman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 10:15:08 by aherrman          #+#    #+#             */
-/*   Updated: 2023/11/03 14:29:07 by aherrman         ###   ########.fr       */
+/*   Updated: 2023/11/06 11:17:14 by aherrman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	write_error(char *cmd, char *input, int error_no)
 	int		len;
 	char	*err_str;
 
-	write(STDERR_FILENO, "Minishell: ", 11);
+	write(STDERR_FILENO, "minishell ", 11);
 	if (cmd != NULL)
 	{
 		len = ft_strlen(cmd);
@@ -35,15 +35,22 @@ void	write_error(char *cmd, char *input, int error_no)
 	write(STDERR_FILENO, err_str, len);
 	write(STDERR_FILENO, "\n", 2);
 }
-void	write_preset_error(char *err, char *cmd)
+void	write_preset_error(char *err, char *input, char *cmd)
 {
 	int	len;
 
+	write(STDERR_FILENO, "minishell ", 11);
 	len = 0;
 	if (cmd != NULL)
 	{
 		len = ft_strlen(cmd);
 		write(STDERR_FILENO, cmd, len);
+		write(STDERR_FILENO, ": ", 2);
+	}
+	if (input != NULL)
+	{
+		len = ft_strlen(input);
+		write(STDERR_FILENO, input, len);
 		write(STDERR_FILENO, ": ", 2);
 	}
 	if (err != NULL)
@@ -59,15 +66,17 @@ int	error(char *cmd, char *input, int error)
 	if (error == 0)
 		return (error);
 	else if (error == 1)
-		write_preset_error("No such file or directory", cmd);
+		write_preset_error("No such file or directory", input, cmd);
 	else if (error == 2)
-		write_preset_error("`=': not a valid identifier", cmd);
+		write_preset_error("`=': not a valid identifier", input, cmd);
 	else if (error == 3)
-		write_preset_error("`=': : HOME not set", cmd);
+		write_preset_error(": HOME not set", input, cmd);
 	else if (error == 4)
-		write_preset_error("`=': : HOME not set", cmd);
+		write_preset_error(" too many arguments", input, cmd);
+	else if (error == 5)
+		write_preset_error("numeric argument required", input, cmd);
 	else if (error == 127)
-		write_preset_error("command not found", cmd);
+		write_preset_error("command not found", input, cmd);
 	else
 		write_error(cmd, input, error);
 	return (errno);
