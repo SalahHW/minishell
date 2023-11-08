379a99839b28df723b7f3c95cc6ca0e14290fe35
Salah <salah.bouheniche@gmail.com>
2023-11-08 16:04:25 +0100

    Fix norm

--- src/prompt/prompt.c
+++ src/prompt/prompt.c
@@ -6,7 +6,7 @@
 /*   By: aherrman <aherrman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 00:07:11 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/11/08 14:36:47 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/11/08 16:02:59 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,9 @@
 
 void	read_user_input(t_shell *shell)
 {
-	char *input;
-	int status_code;
+	char	*input;
+	int		status_code;
 
-	rl_catch_signals = 0;
 	while (shell->general->status)
 	{
 		signal_handlers(READ);
@@ -26,19 +25,14 @@ void	read_user_input(t_shell *shell)
 		{
 			if (*input)
 				add_history(input);
-			if (!g_sigquit_received)
-			{
-				tokenizer(input, shell);
-				// print_tokens_list(shell->tokens);
-				status_code = analyze_tokens(shell->tokens);
-				if (!status_code)
-					execute_cmd(shell);
-				else
-					shell->last_exit_code = status_code;
-			}
+			tokenizer(input, shell);
+			status_code = analyze_tokens(shell->tokens);
+			if (!status_code)
+				execute_cmd(shell);
+			else
+				shell->last_exit_code = status_code;
 			free(input);
-			if (!g_sigquit_received)
-				clear_tokens_list(shell->tokens);
+			clear_tokens_list(shell->tokens);
 		}
 		if (!input)
 			shell->general->status = 0;
