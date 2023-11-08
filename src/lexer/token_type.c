eeb7d46268617d412b9de074689a510c6744914b
Salah <salah.bouheniche@gmail.com>
2023-11-08 15:01:56 +0100

    Refactor token type detection in lexer

--- src/lexer/token_type.c
+++ src/lexer/token_type.c
@@ -6,13 +6,13 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 04:43:28 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/11/01 20:47:18 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/11/08 14:36:37 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-static t_tokentype	get_token_type(t_token *token)
+static t_tokentype	get_token_type(t_token *token, int *cmd_encountered)
 {
 	if (!token->quote && ft_strncmp(token->value, "|", 2) == 0)
 		return (t_pipe);
@@ -24,18 +24,29 @@ static t_tokentype	get_token_type(t_token *token)
 		return (t_redirect_append);
 	if (!token->quote && ft_strncmp(token->value, "<<", 3) == 0)
 		return (t_heredoc);
-	if (!token->prev || token->prev->type == t_pipe
-		|| token->prev->type == t_file)
+	if (!token->prev || token->prev->type == t_pipe)
+	{
+		*cmd_encountered = 1;
 		return (t_cmd);
+	}
 	if (token->prev->type == t_redirect_in
 		|| token->prev->type == t_redirect_out
 		|| token->prev->type == t_redirect_append
 		|| token->prev->type == t_heredoc)
 		return (t_file);
+	if (!token->prev && token->prev->type == t_file)
+	{
+		if (!*cmd_encountered)
+			return (t_cmd);
+	}
 	return (t_arg);
 }
 
 void	detect_tokens_type(t_token *token)
 {
-	token->type = get_token_type(token);
+	static int	cmd_encountered;
+
+	token->type = get_token_type(token, &cmd_encountered);
+	if (token->type == t_pipe)
+		cmd_encountered = 0;
 }
